f4b0c84d0d92a7c375a935f3fe02ec456603f049
Evgeni67 <evgeni313@abv.bg>
2021-03-19 15:36:16 +0500

    update

--- vidima-bg/src/components/login.jsx
+++ vidima-bg/src/components/login.jsx
@@ -10,11 +10,42 @@ import {
   Container,
 } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import axios from "axios";
 import "./styles/login.css";
 
 import { FcGoogle } from "react-icons/fc";
 class Login extends Component {
+  state = {
+    email: "",
+    password: "",
+  };
+  changeEmail = (e) => {
+    this.setState({ email: e.currentTarget.value });
+  };
+  changePassword = (e) => {
+    this.setState({ password: e.target.value });
+  };
+  login = async () => {
+    const url = process.env.REACT_APP_URL;
+    this.setState({ loading: true });
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      data: {
+        email: this.state.email,
+        password: this.state.password,
+      },
+    };
+    const res = await axios(url + "/profile/login", requestOptions);
+    if (res.status === 200) {
+      console.log(res);
+      localStorage.setItem('accessToken', res.data.accessToken);
+  localStorage.setItem('refreshToken', res.data.refreshToken);
+      window.alert("logged");
+    } else {
+      console.log(res);
+    }
+  };
   render() {
     return (
       <>
@@ -29,6 +60,7 @@ class Login extends Component {
               id="fname"
               name="fname"
               defaultValue="name@email.com"
+              onChange={(e) => this.changeEmail(e)}
             />
           </Row>
           <Row className="d-flex justify-content-center">
@@ -39,10 +71,11 @@ class Login extends Component {
               id="fname"
               name="fname"
               defaultValue="password"
+              onChange={(e) => this.changePassword(e)}
             />
           </Row>
           <Row className="loginBtnRow d-flex justify-content-center mt-3">
-            <button className="loginBtn">Login </button>
+            <button className="loginBtn" onClick = {() => this.login()}>Login </button>
           </Row>
           <Row className="loginGoogleRow d-flex justify-content-center mt-3">
             <FcGoogle className="googleIcon" />{" "}
@@ -55,8 +88,9 @@ class Login extends Component {
         </Container>
         <Container className="loginRegisterContainer">
           <Row className="loginRegisterRow d-flex justify-content-center">
-            <p>Do not have an account? </p>{" "}<Link to = "/register">
-            <p className="loginRegisterText">Register</p>
+            <p>Do not have an account? </p>{" "}
+            <Link to="/register">
+              <p className="loginRegisterText">Register</p>
             </Link>
           </Row>
         </Container>
--- vidima-bg/src/components/register.jsx
+++ vidima-bg/src/components/register.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 import {
   Navbar,
   Nav,
@@ -12,76 +13,124 @@ import {
 import "./styles/register.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 class Register extends Component {
+  state = {
+    email: "",
+    password: "",
+    confirmPassowrd: "",
+    address: "",
+    phoneNumber: "",
+  };
+  changeEmail = (e) => {
+    this.setState({ email: e.currentTarget.value });
+  };
+  changePassword = (e) => {
+    this.setState({ password: e.target.value });
+  };
+  changeConfirmPassword = (e) => {
+    this.setState({ confirmPassowrd: e.target.value });
+  };
+  changeAddress = (e) => {
+    this.setState({ address: e.target.value });
+  };
+  changePhone = (e) => {
+    this.setState({ phoneNumber: e.target.value });
+  };
+  register = async () => {
+    const url = process.env.REACT_APP_URL;
+    this.setState({ loading: true });
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      data: {
+        email: this.state.email,
+        password: this.state.password,
+        address: this.state.address,
+        phoneNumber: this.state.phoneNumber
+      },
+    };
+    if(this.state.password === this.state.confirmPassowrd){
+      const res = await axios(url + "/profile/register", requestOptions);
+      if (res.status === 200) {
+        window.alert("registered")
+      } else {
+        console.log(res);
+      }
+    }else{
+      window.alert("Password does not match")
+    }
+   
+
+  
+  };
   render() {
     return (
       <>
-       <Row className="d-flex justify-content-center">
-               <p className = "regLogo">L.O.G.O. </p>
-                </Row>
-          <Container className="regCredentailsContainer">
-            <Row className="mainRegRow">
-              <Col sm={5}>
-                <Row className="d-flex justify-content-center">
-                  <input
-                    className="emailRegister text-align-center"
-                    type="text"
-                    id="fname"
-                    name="fname"
-                    defaultValue="name@email.com"
-                  />
-                </Row>
+        <Row className="d-flex justify-content-center">
+          <p className="regLogo">L.O.G.O. </p>
+        </Row>
+        <Container className="regCredentailsContainer">
+          <Row className="mainRegRow">
+            <Col sm={5}>
+              <Row className="d-flex justify-content-center">
+                <input
+                  className="emailRegister text-align-center"
+                  type="text"
+                  name="fname"
+                  defaultValue="name@email.com"
+                  onChange={(e) => this.changeEmail(e)}
+                />
+              </Row>
 
-                <Row className="d-flex justify-content-center">
-                  <input
-                    className="passwordRegister text-align-center"
-                    type="password"
-                    id="fname"
-                    name="fname"
-                    defaultValue="name@email.com"
-                  />
-                </Row>
-                <Row className="d-flex justify-content-center">
-                  <input
-                    className="confirmPassword text-align-center"
-                    type="password"
-                    defaultValue="name@email.com"
-                  />
-                </Row>
-              </Col>
-              <Col sm={2}>
-                 
-                <Row className="d-flex justify-content-center">
-                <button className="regBtn">Register</button>
-                </Row>
-              </Col>
-              <Col sm={5}>
-                <Row className="d-flex justify-content-center">
-                  <input
-                    className="mobileRegister text-align-center"
-                    type="text"
-                    defaultValue="+359 . . . . . . . . ."
-                  />
-                </Row>
-                
-                <Row className="d-flex justify-content-center">
-                  <input
-                    className="addressRegister text-align-center"
-                    type="text"
-                    defaultValue="Address"
-                  />
-                </Row>
-                <Row className="registerToLogin d-flex justify-content-center">
-                  <p className = "registerQ" 
-                  >
-                      Already have an account? 
-                      </p>
-                      <Link to="/login">
-                      <p className = "registerLogin"> Login</p>
-                      </Link>
-                </Row>
-              </Col>
-            </Row>
-          </Container>
+              <Row className="d-flex justify-content-center">
+                <input
+                  className="passwordRegister text-align-center"
+                  type="password"
+                  name="fname"
+                  defaultValue="password"
+                  onChange={(e) => this.changePassword(e)}
+                />
+              </Row>
+              <Row className="d-flex justify-content-center">
+                <input
+                  className="confirmPassword text-align-center"
+                  type="password"
+                  defaultValue="password"
+                  onChange={(e) => this.changeConfirmPassword(e)}
+                />
+              </Row>
+            </Col>
+            <Col sm={2}>
+              <Row className="d-flex justify-content-center">
+                <button className="regBtn" onClick = {() => this.register()}>Register</button>
+              </Row>
+            </Col>
+            <Col sm={5}>
+              <Row className="d-flex justify-content-center">
+                <input
+                  className="mobileRegister text-align-center"
+                  type="text"
+                  defaultValue="+359 . . . . . . . . ."
+                  onChange={(e) => this.changePhone(e)}
+                />
+              </Row>
+
+              <Row className="d-flex justify-content-center">
+                <input
+                  className="addressRegister text-align-center"
+                  type="text"
+                  defaultValue="Address"
+                  onChange={(e) => this.changeAddress(e)}
+                />
+              </Row>
+              <Row className="registerToLogin d-flex justify-content-center">
+                <p className="registerQ">Already have an account?</p>
+                <Link to="/login">
+                  <p className="registerLogin" > Login</p>
+                </Link>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
       </>
     );
   }
